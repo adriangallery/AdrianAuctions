372ce4abe70edfd335b1bdbccc8063ce6b55ef4f
ADRIAN <adriangallery@adriancerda.com>
2025-05-13 18:06:00 +0200

    Simplificado a una única página HTML estática con Bootstrap que incluye todas las funcionalidades

--- public/test.js
+++
@@ -1,8 +0,0 @@
-console.log('JavaScript está funcionando correctamente');
-
-// Creamos un elemento para mostrar en la página
-window.onload = function() {
-  if (document.getElementById('js-test')) {
-    document.getElementById('js-test').textContent = 'JavaScript está funcionando correctamente';
-  }
-}; 
